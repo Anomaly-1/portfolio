eebef1e4ccb9c36e32a274cc25fb4e544f44c106
Anomaly-1 <cognitivetech52@gmail.com>
2025-03-23 22:31:59 -0500

    base

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable specific rules
+      "react/no-unescaped-entities": "off", // Disable unescaped entities rule
+      "prefer-const": "off", // Disable prefer-const rule
+      "@typescript-eslint/no-unused-vars": "off", // Disable unused vars rule
+      "@typescript-eslint/no-explicit-any": "off", // Disable no-explicit-any rule
+      "react-hooks/exhaustive-deps": "off", // Disable exhaustive-deps rule
+      "no-var": "off", // Disable no-var rule
+    },
+  },
 ];
 
 export default eslintConfig;
